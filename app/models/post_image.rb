f57ceab9178c5538a086341ec80b0100348df360
kobayashi9127 <bacchi.9127@gmail.com>
2021-04-16 13:26:44 +0000

    [error]

---
+++ app/models/post_image.rb
@@ -0,0 +1,3 @@
+class PostImage < ApplicationRecord
+  attachment :image
+end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'd7129b737c799bbe8ff67c81e6b2f5599e04f4a17d162b2eed30c2c8e311d106c2fdb9c0b6668fbf4e07ab886219f89a07900738e4d4c7b6732c0ad4ec626816'
